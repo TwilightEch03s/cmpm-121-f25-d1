bb0d55efc24d55d612bea38a285c94e2868624f7
Lorenzo Uk <103523532+TwilightEch03s@users.noreply.github.com>
2025-10-10 03:02:36 +0000

    step #3 completed! added setInterval so counter increments by 1 each second

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,17 @@ display.textContent = "Stolen Cash $" + money;
 const button = document.createElement("button");
 button.textContent = "💵";
 
+// On click increment
 button.addEventListener("click", () => {
   money += 1;
   display.textContent = "Stolen Cash $" + money;
 });
+
+//setInterval
+setInterval(() => {
+  money += 1;
+  display.textContent = "Stolen Cash $" + money;
+}, 1000);
 
 // Add everything to the page
 document.body.appendChild(display);
