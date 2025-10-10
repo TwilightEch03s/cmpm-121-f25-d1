eafbd786d9ae346a628c30a3d38e71733479bdda
Lorenzo Uk <103523532+TwilightEch03s@users.noreply.github.com>
2025-10-10 02:49:34 +0000

    added increment to the button for stolen cash!

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import "./style.css";
 
+let money = 0;
+
+// Create div
+const display = document.createElement("div");
+display.textContent = "Stolen Cash $" + money;
+
+// Add div to browser
+document.body.appendChild(display);
+
 // Create a new button
 const button = document.createElement("button");
 button.textContent = "💵";
+
+button.addEventListener("click", () => {
+  money += 1;
+  display.textContent = "Stolen Cash $" + money;
+});
 
 // Add the button to the page
 document.body.appendChild(button);
